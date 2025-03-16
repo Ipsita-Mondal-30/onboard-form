8b475fd89f0db728302577c24aad1819d2d2b74c
Ipsita Mondal <ipsitaamondal@gmail.com>
2025-03-16 16:12:45 +0530

    Added css styles for the form

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="bg-white w-full h-screen inline-table">{children}</main>
       </body>
     </html>
   );
